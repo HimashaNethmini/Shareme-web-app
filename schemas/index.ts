15b68783b182cca0407b235389741181f91ea39f
HimashaNethmini <himashanethmini05@gmail.com>
2023-02-22 14:10:16 +0530

    Shareme frontend

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+// schemas/index.js
+import user from './user'
+import pin from './pin'
+import comment from './comment'
+import save from './save'
+import postedBy from './postedBy'
+
+export const schemaTypes = [user,pin,comment,save,postedBy]
